285498fe3f8c0dc921beef54686ccac1ecb8e79f
jardel88 <jardelsilvagomes@hotmail.com>
2022-08-08 11:49:34 -0300

    Utilizando Lambda Layer na funcao de Pesquisa e ADM de produtos

--- lambda/products/ProductsAdminFunction.ts
+++ lambda/products/ProductsAdminFunction.ts
@@ -1,4 +1,11 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import { Product, ProductRepository } from "/opt/nodejs/productLayer";
+import { DynamoDB } from "aws-sdk";
+
+const productsDdb = process.env.PRODUCTS_DDB!
+const ddbClient = new DynamoDB.DocumentClient
+
+const productRepository = new ProductRepository(ddbClient, productsDdb)
 
 export async function handler (event: APIGatewayProxyEvent,
     context: Context): Promise<APIGatewayProxyResult> {
@@ -10,25 +17,50 @@ export async function handler (event: APIGatewayProxyEvent,
 
     if (event.resource === "/products") {
         console.log("POST /products") 
+        const product = JSON.parse(event.body!) as Product
+        const productCreated = await productRepository.create(product)
+
         return {
             statusCode: 201,
-            body: "POST /products"
+            body: JSON.stringify(productCreated)
         }
             
     } else if (event.resource === "/products/{id}"){
-        const productsId = event.pathParameters!.id as string
+        const productId = event.pathParameters!.id as string
         if (event.httpMethod === "PUT"){
-        console.log(`PUT /products/${productsId}`) 
+        console.log(`PUT /products/${productId}`) 
+        const product = JSON.parse(event.body!) as Product
+
+        try {
+            const productUpdated = await productRepository.updateProduct(productId, product)
+        
             return {
                 statusCode: 200,
-                body: `PUT /products/${productsId}`
+                body: JSON.stringify(productUpdated)
             }
+        } catch (ConditionalCheckFailedException) {
+            return {
+                statusCode: 404,
+                body: 'Product not found'
+            }
+        }
+        
     } else if (event.httpMethod === "DELETE"){
-        console.log(`DELETE /products/${productsId}`) 
+        console.log(`DELETE /products/${productId}`) 
+        try {
+            const product = await productRepository.deleteProduct(productId)
             return {
                 statusCode: 200,
-                body: `DELETE /products/${productsId}`
+                body: JSON.stringify(product)
             }
+        } catch (error){
+            console.error((<Error>error).message)
+                return {
+                    statusCode: 404,
+                    body: (<Error>error).message
+                }
+            }
+           
         }
     }
 
--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -1,6 +1,12 @@
 import { EventSourceMapping } from "aws-cdk-lib/aws-lambda";
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import { ProductRepository } from "/opt/nodejs/productLayer";
+import { DynamoDB } from "aws-sdk";
 
+const productsDdb = process.env.PRODUCTS_DDB!
+const ddbClient = new DynamoDB.DocumentClient
+
+const productRepository = new ProductRepository(ddbClient, productsDdb)
 
 export async function handler (event: APIGatewayProxyEvent,
     context: Context): Promise<APIGatewayProxyResult> {
@@ -13,22 +19,33 @@ export async function handler (event: APIGatewayProxyEvent,
         const method = event.httpMethod
         if(event.resource === "/products"){
             if (method === "GET") {
-                console.log('GET')
+                console.log('GET /products')
+
+                const products = await productRepository.getAllProducts
 
                 return {
                     statusCode: 200,
-                    body:JSON.stringify({
-                        message: "GET Products - OK"
-                    })
+                    body: JSON.stringify(products)
                 }
             }
         } else if (event.resource === "/products/{id}"){
             const productId = event.pathParameters!.id as string
             console.log(`GET /products/${productId}`)
-            return {
-                statusCode: 200,
-                body: `GET /products/${productId}`
+
+            try {
+                const product = await productRepository.getProductById(productId)
+                return {
+                 statusCode: 200,
+                 body: JSON.stringify(product)
+                }
+            } catch (error) {
+                console.error((<Error>error).message)
+                return {
+                    statusCode: 404,
+                    body: (<Error>error).message
+                }
             }
+            
         }
 
         return {
