d2739a849111a134eda4a2d1b3c02e4b08c4dad1
jardel88 <jardelsilvagomes@hotmail.com>
2022-08-16 14:31:01 -0300

    Add servico de pedidos ao API Gateway existente

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -5,8 +5,9 @@ import * as cwlogs from "aws-cdk-lib/aws-logs"
 import { Construct } from "constructs"
 
 interface ECommerceApiStackProps extends cdk.StackProps {
-    productsFetchHandler: lambdaNodeJs.NodejsFunction
-    productsAdminHandler: lambdaNodeJs.NodejsFunction
+    productsFetchHandler: lambdaNodeJs.NodejsFunction;
+    productsAdminHandler: lambdaNodeJs.NodejsFunction;
+    ordersHandler: lambdaNodeJs.NodejsFunction;
 }
 
 export class ECommerceApiStack extends cdk.Stack {
@@ -34,8 +35,32 @@ export class ECommerceApiStack extends cdk.Stack {
             }
         })
 
+        this.createProductsService(props, api)
+
+        this.createOrdersService(props, api)
+    }
+
+    private createOrdersService(props: ECommerceApiStackProps, api: apigateway.RestApi) {
+        const ordersIntegration = new apigateway.LambdaIntegration(props.ordersHandler)
+
+        //resource - /orders
+        const ordersResource = api.root.addResource('orders')
+
+        //GET /orders
+        //GET /orders?email=test@email.com
+        //GET /orders?email=test@email.com&orderId
+        ordersResource.addMethod("GET", ordersIntegration)
+
+        //DELETE /orders?email=test@email.com&orderId
+        ordersResource.addMethod("DELETE", ordersIntegration)
+
+        //POST /orders
+        ordersResource.addMethod("POST", ordersIntegration)
+    }
+
+    private createProductsService(props: ECommerceApiStackProps, api: apigateway.RestApi) {
         const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
-        
+
         //"/products"
         const productsResourcce = api.root.addResource("products")
         productsResourcce.addMethod("GET", productsFetchIntegration)
@@ -49,7 +74,7 @@ export class ECommerceApiStack extends cdk.Stack {
         // POST "/products"
         productsResourcce.addMethod("POST", productsAdminIntegration)
 
-         // PUT "/products/{id}"
+        // PUT "/products/{id}"
         productIdResource.addMethod("PUT", productsAdminIntegration)
 
         // DELETE "/products/{id}"
