a3f17a0663390e8a8404ccbf6553b465603d74f5
jardel88 <jardelsilvagomes@hotmail.com>
2022-08-17 16:49:38 -0300

    Metodo para construcao do modelo e resposta de pedido

--- lambda/orders/ordersFunction.ts
+++ lambda/orders/ordersFunction.ts
@@ -1,7 +1,8 @@
+import { OrderRequest, OrderProductResponse, OrderResponse, PaymentType, ShippingType, CarrierType } from './layers/ordersApiLayer/nodejs/orderApi';
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 import { DynamoDB } from "aws-sdk";
-import { OrderRepository } from "/opt/nodejs/ordersLayer";
-import { ProductRepository } from "/opt/nodejs/productsLayer";
+import { Order, OrderRepository } from "/opt/nodejs/ordersLayer";
+import { Product, ProductRepository } from "/opt/nodejs/productsLayer";
 import * as AWSXRay from "aws-xray-sdk";
 
 AWSXRay.captureAWS(require("aws-sdk"))
@@ -50,4 +51,56 @@ export async function handler(event: APIGatewayProxyEvent, context: Context): Pr
         statusCode: 400,
         body: 'Bad Request'
     }
+}
+
+function convertToOrderResponse (order: Order): OrderResponse {
+    const orderProducts: OrderProductResponse[] = []
+    order.products.forEach((product) => {
+        orderProducts.push({
+            code: product.code,
+            price: product.price
+        })
+    })
+    const orderResponse: OrderResponse = {
+        email: order.pk,
+        id: order.sk!,
+        createdAt: order.createdAt!,
+        products: orderProducts,
+        billing:{
+            payment: order.billing.payment as PaymentType,
+            totalPrice: order.billing.totalPrice
+        },
+        shipping: {
+            type: order.shipping.type as ShippingType,
+            carrier: order.shipping.carrier as CarrierType
+        }
+    }
+    return orderResponse
+}
+
+function buildOrder(orderRequest: OrderRequest, products: Product[]): Order {
+    const orderProducts: OrderProductResponse[] = []
+    let totalPrice = 0
+
+    products.forEach((product) => {
+        totalPrice += product.price
+        orderProducts.push({
+            code: product.code,
+            price: product.price
+        })
+    })
+
+    const order: Order = {
+        pk: orderRequest.email,
+        billing: {
+            payment: orderRequest.payment,
+            totalPrice: totalPrice
+        },
+        shipping: {
+            type: orderRequest.shipping.type,
+            carrier: orderRequest.shipping.carrier
+        },
+        products: orderProducts
+    }
+    return order
 }
