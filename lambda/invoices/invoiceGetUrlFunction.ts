190803d075bc4f9f3e60384920cbd506a59c5e25
jardel88 <jardelsilvagomes@hotmail.com>
2022-08-29 11:53:05 -0300

    Criacao repositorio de transacoes de notas fiscais

--- lambda/invoices/invoiceGetUrlFunction.ts
+++ lambda/invoices/invoiceGetUrlFunction.ts
@@ -2,6 +2,7 @@ import { ApiGatewayManagementApi, DynamoDB, S3 } from "aws-sdk";
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 import * as AWSXRay from "aws-xray-sdk"
 import { v4 as uuid } from "uuid"
+import { InvoiceTransactionStatus, InvoiceTransactionRepository } from './layers/invoiceTransaction/nodejs/invoiceTransaction';
 
 AWSXRay.captureAWS(require('aws-sdk'))
 
@@ -14,6 +15,8 @@ const ddbClient = new DynamoDB.DocumentClient()
 const apigwManagementApi = new ApiGatewayManagementApi({
     endpoint: invoicesWsApiEndpoint
 })
+
+const invoiceTransactionRepository = new InvoiceTransactionRepository(ddbClient, invoicesDdb)
 
 export async function handler (event: APIGatewayProxyEvent, context: Context): 
     Promise<APIGatewayProxyResult> {
@@ -33,6 +36,24 @@ export async function handler (event: APIGatewayProxyEvent, context: Context):
         Key: key,
         Expires: expires
     })
+
+    //Create invoice transaction 
+    const timestamp = Date.now()
+    const ttl = ~~(timestamp / 1000 + 60 * 2)
+    await invoiceTransactionRepository.createInvoiceTransaction({
+        pk: "#transaction",
+        sk: key,
+        ttl: ttl,
+        requestId: lambdaRequestId,
+        transactionStatus: InvoiceTransactionStatus.GENERATED,
+        timestamp: timestamp,
+        expiresIn: expires,
+        connectionId: connectionId,
+        endpoint: invoicesWsApiEndpoint
+    })
+
+    //Send URL back to WS connected client
+
     return {
         statusCode: 200,
         body: 'OK'
